70fdeddff2fcfebe0833ae64ca8190f8a00bc6fa
dkluger <dkluger@blackrockneuro.com>
2024-05-08 11:11:08 -0600

    Modified brMiscFxns.py

--- brpylib/brMiscFxns.py
+++ brpylib/brMiscFxns.py
@@ -1,6 +1,6 @@
 """
 Random functions that may be useful elsewhere (or necessary)
-current version: 1.2.0 --- 08/04/2016
+current version: 1.3.0 --- 05/08/2024
 
 @author: Mitch Frankel - Blackrock Microsystems
 
@@ -8,18 +8,14 @@ Version History:
 v1.0.0 - 07/05/2016 - initial release
 v1.1.0 - 07/12/2016 - minor editing changes to print statements and addition of version control
 v1.2.0 - 08/04/2016 - minor modifications to allow use of Python 2.6+
+v1.3.0 - 05/08/2024 - removed Qt dependency, replaced with tkinter
 """
 from os import getcwd, path
-
-try:
-    from qtpy.QtWidgets import QApplication, QFileDialog
-
-    HAS_QT = True
-except ModuleNotFoundError:
-    HAS_QT = False
+from tkinter import Tk
+from tkinter.filedialog import askopenfile
 
 # Version control
-brmiscfxns_ver = "1.2.0"
+brmiscfxns_ver = "1.3.0"
 
 # Patch for use with Python 2.6+
 try:
@@ -39,28 +35,16 @@ def openfilecheck(open_mode, file_name="", file_ext="", file_type=""):
 
     while True:
         if not file_name:  # no file name passed
-            if not HAS_QT:
-                raise ModuleNotFoundError(
-                    "Qt required for file dialog. Install PySide + qtpy or provide file_name."
-                )
-
+            if not file_type: # no extension passed
+                file_type = ("*.ns1", "*.ns2", "*.ns3", "*.ns4", "*.ns5", "*.ns6")
             # Ask user to specify a file path or browse
-            file_name = input(
-                "Enter complete " + file_ext + " file path or hit enter to browse: "
-            )
-
-            if not file_name:
-                if "app" not in locals():
-                    app = QApplication([])
-                if not file_ext:
-                    file_type = "All Files"
-                file_name = QFileDialog.getOpenFileName(
-                    QFileDialog(),
-                    "Select File",
-                    getcwd(),
-                    file_type + " (*" + file_ext + ")",
+            Tk().withdraw()
+            file_name = askopenfile(
+                title="Select original file",
+                initialdir=getcwd(),
+                filetypes=[("NSx Files", file_type)],
                 )
-                file_name = file_name[0]
+            file_name = file_name.name
 
         # Ensure file exists (really needed for users type entering)
         if path.isfile(file_name):
